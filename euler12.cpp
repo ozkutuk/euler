0a9065e9e6038decee111e479cf56b3b19ab3289
Berk Özkütük <berkobrathestabber@gmail.com>
2018-04-10 15:19:16 +0300

    optimized to check until sqrt, not half

--- euler12.cpp
+++ euler12.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
-inline int nth_triangle(int n) {
-    return n * (n+1) / 2;
-}
-
-int count_divisors(int number) {
-    std::vector<int> divisor_test(number/2, 1);
+int count_divisors(long long  number) {
+    std::vector<long long> divisor_test(std::ceil(std::sqrt(number)), 1);
     std::size_t divisor_count = 0;
     std::size_t i = 0;
     while(i < divisor_test.size()) {
         if(divisor_test[i] == 1) {
             int divisor = i + 1;
-            if(number % divisor == 0)
-                divisor_count++;
+            if(number % divisor == 0) {
+                divisor_count += 2;
+            }
             else {
-                int non_divisor = divisor;
-                while(non_divisor < number/2 + 1) {
+                long long non_divisor = divisor;
+                while(non_divisor < divisor_test.size()) {
                     divisor_test[non_divisor - 1] = 0;
                     non_divisor += divisor;
                 }
@@ -32,10 +30,8 @@ int triangle_over_x_divisors(int x) {
     int triangle_index = 1;
     int triangle = 0;
     while(divisor_count <= x) {
-        // triangle = nth_triangle(triangle_index);
         triangle += triangle_index;
         divisor_count = count_divisors(triangle);
-        std::cout << triangle << ' ' << divisor_count << std::endl;
         triangle_index++;
     }
     return triangle;
--- euler12.py
+++ euler12.py
@@ -8,7 +8,7 @@ def next_triangle():
         yield nth_triangle(n)
     
 def count_divisors(number):
-    divisor_test = [1] * (number // 2) # from 2 to number/2 (inclusive)
+    divisor_test = [1] * int(number ** 0.5)
     divisor_count = 0
     i = 0
     while i < len(divisor_test):
@@ -16,19 +16,16 @@ def count_divisors(number):
             # test divisibility by i+1 (0th index corresponds to 1 and so on)
             divisor = i + 1
             if number % divisor == 0:
-                # print(divisor, 'is a divisor')
-                divisor_count += 1
+                divisor_count += 2
             else:
-                for non_divisor in range(divisor, number // 2 + 1, divisor):
+                for non_divisor in range(divisor, len(divisor_test), divisor):
                     divisor_test[non_divisor - 1] = 0
-                    # print(non_divisor, 'is not a divisor')
         i += 1
     return divisor_count + 1
 
 def triangle_over_x_divisors(x):
     for triangle in next_triangle():
         a = count_divisors(triangle)
-        print(triangle, a)
         if a > x:
             return triangle
 
